bc4b64437fb57e49d3191e79356e4fa96957437b
HaiYangWu <wuhaiyang1213@gmail.com>
2019-10-31 23:34:52 +0800

    message

---
+++ protoo-client/src/androidTest/java/org/protoojs/droid/MessageTest.java
@@ -0,0 +1,51 @@
+package org.protoojs.droid;
+
+import org.json.JSONObject;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MessageTest {
+
+  private static final String METHOD_TEST = "test";
+  private static final long ERROR_CODE_TEST = 1;
+  private static final String ERROR_REASON_TEST = "test error code";
+
+  @Test
+  public void createRequestResponse() {
+    // test createRequest and parse request.
+    JSONObject requestObj = Message.createRequest(METHOD_TEST, null);
+    Message parsedRequest = Message.parse(requestObj.toString());
+    assertTrue(parsedRequest instanceof Message.Request);
+
+    Message.Request request = (Message.Request) parsedRequest;
+    assertEquals(METHOD_TEST, request.getMethod());
+
+    // test createSuccessResponse and parse success response.
+    JSONObject sucResObj = Message.createSuccessResponse(request, null);
+    Message parsedSucRes = Message.parse(sucResObj.toString());
+    assertTrue(parsedSucRes instanceof Message.Response);
+
+    Message.Response sucRes = (Message.Response) parsedSucRes;
+    assertEquals(request.getId(), sucRes.getId());
+
+    // test createErrorResponse and parse error response.
+    JSONObject errResObj = Message.createErrorResponse(request, ERROR_CODE_TEST, ERROR_REASON_TEST);
+    Message parsedErrRes = Message.parse(errResObj.toString());
+    assertTrue(parsedErrRes instanceof Message.Response);
+
+    Message.Response errRes = (Message.Response) parsedErrRes;
+    assertEquals(request.getId(), errRes.getId());
+    assertEquals(ERROR_CODE_TEST, errRes.getErrorCode());
+    assertEquals(ERROR_REASON_TEST, errRes.getErrorReason());
+  }
+
+  @Test
+  public void createNotification() {
+    JSONObject request = Message.createNotification(METHOD_TEST, null);
+    Message parsedMessage = Message.parse(request.toString());
+    assertTrue(parsedMessage instanceof Message.Notification);
+    assertEquals(METHOD_TEST, ((Message.Notification) parsedMessage).getMethod());
+  }
+}
---
+++ protoo-client/src/main/java/org/protoojs/droid/Message.java
@@ -0,0 +1,266 @@
+package org.protoojs.droid;
+
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Message {
+
+  private static final String TAG = "message";
+
+  // message data.
+  private JSONObject mData;
+
+  public Message() {}
+
+  public Message(JSONObject data) {
+    mData = data;
+  }
+
+  public JSONObject getData() {
+    return mData;
+  }
+
+  public void setData(JSONObject data) {
+    mData = data;
+  }
+
+  public static class Request extends Message {
+
+    private boolean mRequest = true;
+    private String mMethod;
+    private long mId;
+
+    public Request(String method, long id, JSONObject data) {
+      super(data);
+      mMethod = method;
+      mId = id;
+    }
+
+    public boolean isRequest() {
+      return mRequest;
+    }
+
+    public void setRequest(boolean request) {
+      mRequest = request;
+    }
+
+    public long getId() {
+      return mId;
+    }
+
+    public void setId(long id) {
+      mId = id;
+    }
+
+    public String getMethod() {
+      return mMethod;
+    }
+
+    public void setMethod(String method) {
+      mMethod = method;
+    }
+  }
+
+  public static class Response extends Message {
+
+    private boolean mResponse = true;
+    private long mId;
+    private boolean mOK;
+    private long mErrorCode;
+    private String mErrorReason;
+
+    public Response(long id, JSONObject data) {
+      super(data);
+      mId = id;
+      mOK = true;
+    }
+
+    public Response(long id, long errorCode, String errorReason) {
+      mId = id;
+      mOK = false;
+      mErrorCode = errorCode;
+      mErrorReason = errorReason;
+    }
+
+    public boolean isResponse() {
+      return mResponse;
+    }
+
+    public void setResponse(boolean response) {
+      mResponse = response;
+    }
+
+    public long getId() {
+      return mId;
+    }
+
+    public void setId(long id) {
+      mId = id;
+    }
+
+    public boolean isOK() {
+      return mOK;
+    }
+
+    public void setOK(boolean OK) {
+      mOK = OK;
+    }
+
+    public long getErrorCode() {
+      return mErrorCode;
+    }
+
+    public void setErrorCode(long errorCode) {
+      mErrorCode = errorCode;
+    }
+
+    public String getErrorReason() {
+      return mErrorReason;
+    }
+
+    public void setErrorReason(String errorReason) {
+      mErrorReason = errorReason;
+    }
+  }
+
+  public static class Notification extends Message {
+
+    private boolean mNotification = true;
+    private String mMethod;
+
+    public Notification(String method, JSONObject data) {
+      super(data);
+      mMethod = method;
+    }
+
+    public boolean isNotification() {
+      return mNotification;
+    }
+
+    public void setNotification(boolean notification) {
+      mNotification = notification;
+    }
+
+    public String getMethod() {
+      return mMethod;
+    }
+
+    public void setMethod(String method) {
+      mMethod = method;
+    }
+  }
+
+  public static Message parse(String raw) {
+    Logger.d(TAG, "parse()");
+
+    JSONObject object;
+
+    try {
+      object = new JSONObject(raw);
+    } catch (JSONException e) {
+      Logger.e(TAG, String.format("parse() | invalid JSON: %s", e.getMessage()));
+      return null;
+    }
+
+    if (object.optBoolean("request")) {
+      // Request.
+      String method = object.optString("method");
+      long id = object.optLong("id");
+
+      if (TextUtils.isEmpty(method)) {
+        Logger.e(TAG, "parse() | missing/invalid method field");
+        return null;
+      }
+      if (id == 0) {
+        Logger.e(TAG, "parse() | missing/invalid id field");
+        return null;
+      }
+
+      return new Request(method, id, object.optJSONObject("data"));
+    } else if (object.optBoolean("response")) {
+      // Response.
+      long id = object.optLong("id");
+
+      if (id == 0) {
+        Logger.e(TAG, "parse() | missing/invalid id field");
+        return null;
+      }
+
+      if (object.optBoolean("ok")) {
+        return new Response(id, object.optJSONObject("data"));
+      } else {
+        return new Response(id, object.optLong("errorCode"), object.optString("errorReason"));
+      }
+    } else if (object.optBoolean("notification")) {
+      // Notification.
+      String method = object.optString("method");
+
+      if (TextUtils.isEmpty(method)) {
+        Logger.e(TAG, "parse() | missing/invalid method field");
+        return null;
+      }
+
+      return new Notification(method, object.optJSONObject("data"));
+    } else {
+      // Invalid.
+      Logger.e(TAG, "parse() | missing request/response field");
+      return null;
+    }
+  }
+
+  public static JSONObject createRequest(String method, JSONObject data) {
+    JSONObject request = new JSONObject();
+    try {
+      request.put("request", true);
+      request.put("method", method);
+      request.put("id", Utils.generateRandomNumber());
+      request.put("data", data != null ? data : new JSONObject());
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+    return request;
+  }
+
+  public static JSONObject createSuccessResponse(@NonNull Request request, JSONObject data) {
+    JSONObject response = new JSONObject();
+    try {
+      response.put("response", true);
+      response.put("id", request.getId());
+      response.put("ok", true);
+      response.put("data", data != null ? data : new JSONObject());
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+    return response;
+  }
+
+  public static JSONObject createErrorResponse(
+      @NonNull Request request, long errorCode, String errorReason) {
+    JSONObject response = new JSONObject();
+    try {
+      response.put("response", true);
+      response.put("id", request.getId());
+      response.put("ok", false);
+      response.put("errorCode", errorCode);
+      response.put("errorReason", errorReason);
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+    return response;
+  }
+
+  public static JSONObject createNotification(String method, JSONObject data) {
+    JSONObject notification = new JSONObject();
+    try {
+      notification.put("notification", true);
+      notification.put("method", method);
+      notification.put("data", data != null ? data : new JSONObject());
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+    return notification;
+  }
+}
